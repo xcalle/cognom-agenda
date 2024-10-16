17021544fde4d490e1535972a18d30119b97ffa8
Xavi <xaviercalle2004@ginebro.cat>
2024-10-16 17:55:19 +0200

    commit començo a fer el modificar

--- src/aplicacio/AppAgenda.java
+++ src/aplicacio/AppAgenda.java
@@ -1,9 +1,81 @@
 package aplicacio;
 
+import classes.Adressa;
+import classes.Agenda;
+import classes.Contacte;
+import classes.Teclat;
+import eines.EinesJoan;
 import menu.Menu;
 
+import java.util.Scanner;
+
+import static eines.EinesJoan.pintaLinia;
+
 public class AppAgenda {
+    static Agenda agenda = new Agenda();
+
+    public static void mostrarContactes(){
+        agenda.mostrarContactes();
+    }
+
+    public static void afegirContacte(){
+        String nomContacte, cognomContacte;
+        nomContacte = Teclat.llegirCadena("Entra el nom del contacte: ");
+        cognomContacte = Teclat.llegirCadena("Entra el cognom del contacte: ");
+        Contacte nouContacte = new Contacte(nomContacte, cognomContacte);
+        agenda.afegirContacte(nouContacte);
+    }
+
+    public static void buscarContacte(){
+        String nomContacte, cognomContacte;
+        nomContacte = Teclat.llegirCadena("Entra el nom del contacte: ");
+        cognomContacte = Teclat.llegirCadena("Entra el cognom del contacte: ");
+        if(agenda.buscarContacte(nomContacte, cognomContacte)){
+            System.out.println("Contacte trobat!");
+        } else {
+            System.out.println("Contacte no trobat!");
+        };
+    }
+
+    public static void eliminarContacte(){
+        Scanner teclat = new Scanner(System.in);
+        String nomContacte, cognomContacte;
+        char resposta = 'N';
+
+        nomContacte = Teclat.llegirCadena("Entra el nom del contacte: ");
+        cognomContacte = Teclat.llegirCadena("Entra el cognom del contacte: ");
+        Contacte contacteAEsborrar = new Contacte(nomContacte, cognomContacte);
+
+        if(agenda.buscarContacte(nomContacte, cognomContacte)){
+            System.out.print("Estas segur d'eliminar el contacte (S/N): ");
+            resposta = teclat.next().charAt(0);
+            // teclat.close();
+            if ((resposta == 'S') || (resposta == 's') ){
+                agenda.eliminarContacte(contacteAEsborrar);
+                System.out.println("Contacte eliminat correctament!");
+            }
+        } else {
+            System.out.println("Contacte no trobat!");
+        };
+
+    }
+
+    public static void modificarContacte(){
+        String nomContacte, cognomContacte;
+        nomContacte = Teclat.llegirCadena("Entra el nom del contacte: ");
+        cognomContacte = Teclat.llegirCadena("Entra el cognom del contacte: ");
+        if(agenda.buscarContacte(nomContacte, cognomContacte)){
+            System.out.println("Contacte trobat!");
+        } else {
+            System.out.println("Contacte no trobat!");
+        };
+    }
+
+
     public static void main(String[] args) {
+
+
+
         String[] opcions = {
                 "Afegir Contacte",
                 "Buscar Contacte",
@@ -19,19 +91,19 @@ public class AppAgenda {
                     "Escull una opció");
             switch (opcio) {
                 case 1:
-//                    afegirContacte();
+                    afegirContacte();
                     break;
                 case 2:
-//                    buscarContacte();
+                    buscarContacte();
                     break;
                 case 3:
 //                    modificarContacte();
                     break;
                 case 4:
-//                    eliminarContacte();
+                    eliminarContacte();
                     break;
                 case 5:
-//                    mostrarContactes();
+                    mostrarContactes();
                     break;
                 case 6:
 //                    guardarAgenda(nomFitxerAmbAgenda);
--- src/classes/Agenda.java
+++ src/classes/Agenda.java
@@ -1,12 +1,14 @@
 package classes;
 
 import java.util.LinkedList;
+import java.util.Locale;
+import java.util.SplittableRandom;
 
 public class Agenda {
     //Atributs
     private LinkedList<Contacte> llistaContactes;
-    // Constructors
 
+    // Constructors
     public Agenda() {
         this.llistaContactes = new LinkedList<>();
     }
@@ -21,7 +23,53 @@ public class Agenda {
     }
 
     public void afegirContacte(Contacte contacte) {
-        this.llistaContactes.addLast(contacte);
-    }
-}
+        String nomContacte, cognomContacte;
+        boolean existeixElContacte = false;
 
+        nomContacte = contacte.getNom();
+        cognomContacte = contacte.getCognom();
+        existeixElContacte = this.buscarContacte(nomContacte, cognomContacte);
+        if (existeixElContacte) {
+            System.out.println("Contacte amb nom " + contacte.getNom()
+                    + " ja existeix a lagenda!");
+        } else {
+            this.llistaContactes.addLast(contacte);
+            System.out.println("Contacte amb nom " + contacte.getNom()
+                    + " afegit correctament!");
+        }
+    }
+
+    public boolean buscarContacte(String nomContacteABuscar, String cognomContacteABuscar) {
+        boolean trobat = false;
+        if (this.llistaContactes.size() == 0) {
+            trobat = false;
+        } else {
+            for (int i = 0; i < this.getLlistaContactes().size(); i++) {
+                if ((this.llistaContactes.get(i).getNom().equalsIgnoreCase(nomContacteABuscar)) &&
+                        (this.llistaContactes.get(i).getCognom().equalsIgnoreCase(cognomContacteABuscar))) {
+                    trobat = true;
+                }
+            }
+        }
+        return trobat;
+    }
+
+    public void eliminarContacte(Contacte contacte) {
+        this.llistaContactes.remove(contacte);
+    }
+    public void mostrarContactes() {
+        if (this.llistaContactes.size() == 0) {
+            System.out.println("La agenda està buida!");
+        } else {
+            for (int i = 0; i < this.getLlistaContactes().size(); i++) {
+                System.out.println((i + 1) + ".- " + this.llistaContactes.get(i).getNom() +
+                        " " + this.llistaContactes.get(i).getCognom());
+            }
+        }
+//        for (Contacte contacte : this.getLlistaContactes()){
+//            System.out.println("Nom   : " + contacte.getNom());
+//            System.out.println("Cognom: " + contacte.getCognom());
+//        }
+    }
+
+}
